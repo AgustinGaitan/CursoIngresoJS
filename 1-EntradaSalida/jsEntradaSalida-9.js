191b403bf575dbfd89ecc3e449bb09a5ddcad38a
Agustin Gaitan <60585898+AgustinGaitan@users.noreply.github.com>
2020-02-05 11:17:52 -0300

    Ejercicio 9 aumento del 10%
    Ejercicio resuelto.

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,19 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var importe;
+	var resultado;
+
+	importe = document.getElementById('sueldo').value;
+	importe = parseInt(importe);
+	resultado = parseInt(resultado);
+
+	resultado = 10*importe/100+importe
+
+	resultado = document.getElementById('resultado').value=resultado;
+
+	console.log(resultado);
+
 
 	
 }
