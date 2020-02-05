29d6f4b8c61a5b944eeeb64456bfe12dbe711f0d
Agustin Gaitan <60585898+AgustinGaitan@users.noreply.github.com>
2020-02-05 15:46:07 -0300

    Ejercicio 6
    Hecho en casa

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,21 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero = document.getElementById('numeroUno').value;
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = document.getElementById('numeroDos').value;
+	segundoNumero = parseInt(segundoNumero);
+
+	resultado = primerNumero+segundoNumero;
+	resultado = parseInt(resultado);
+
+	alert(resultado);
+
+
 
 }
 
