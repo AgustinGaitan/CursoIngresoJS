45b0df03be9fcba4c3da51d9c57c7c94ab7dfb39
Agustin Gaitan <60585898+AgustinGaitan@users.noreply.github.com>
2020-02-05 10:25:21 -0300

    Ejercicio 7, suma y resta.
    Faltan multiplicar y dividir

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,12 +3,36 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{
+	var primerNumero; //Las variables no pueden: empezar con numeros ni tener espacios//
+	var segundoNumero;
+	var resultado; //las variables van siempre arriba//
+	
+	primerNumero = document.getElementById('numeroUno').value;
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = document.getElementById('numeroDos').value;
+	segundoNumero = parseInt(segundoNumero);
+
+	resultado = primerNumero+segundoNumero; //el signo + concatena, es por eso que se usa parseInt//
+	console.log(resultado);
+
+
+
 	
 }
 
 function restar()
 {
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero = document.getElementById('numeroUno').value;
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = document.getElementById('numeroDos').value;
+	segundoNumero = parseInt(segundoNumero);
+	resultado = primerNumero-segundoNumero;
+	console.log(resultado);
 	
 }
 
