b2d4beead21591c0da492b0a5e549130ef6770ce
Agustin Gaitan <60585898+AgustinGaitan@users.noreply.github.com>
2020-02-05 11:32:52 -0300

    Ejercicio 9 aumento 10% con una variable mas
    Agregue la variable aumento.

---
+++ jsEntradaSalida-9.js
@@ -0,0 +1,24 @@
+/*Debemos lograr tomar el importe por ID ,
+transformarlo a entero (parseInt), luego
+mostrar el importe con un aumento del 10 %
+en el cuadro de texto "RESULTADO".*/
+function mostrarAumento()
+{
+	var importe;
+	var resultado;
+	var aumento;
+	aumento= 10;
+
+	importe = document.getElementById('sueldo').value;
+	importe = parseInt(importe);
+	resultado = parseInt(resultado);
+
+	resultado = aumento*importe/100+importe;
+
+	resultado = document.getElementById('resultado').value=resultado;
+
+	console.log(resultado);
+
+
+	
+}
