57cbcae71e2d0f2c07a642e96e85d5496497c066
Agustin Gaitan <60585898+AgustinGaitan@users.noreply.github.com>
2020-02-11 11:45:19 -0300

    Ejercicio 6 resuelto
    Tiene comentarios con la mejor forma (mas eficiente) de hacerlo.

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,5 +1,76 @@
 function mostrar()
 {
+	var edad;
+	edad=document.getElementById('edad').value;
+    edad=parseInt(edad);
+	if(edad>17)
+	{
+		alert("Usted es mayor de edad");
+
+	}
+	if(edad>12&&edad<18)
+	{
+	 	alert("Usted es adolescente");
+
+	}
+	if(edad<13)
+	{
+		alert("Es un niño");
+	}
+
+	/* Segunda Version
+
+	var edad;
+	edad=document.getElementById('edad').value;
+    edad=parseInt(edad);
+	if(edad>17)
+	{
+		alert("Usted es mayor de edad");
+
+	}
+	else
+  {
+	if(edad>12&&edad<18)
+	{
+	 	alert("Usted es adolescente");
+
+	}
+	if(edad<13)
+	{
+		alert("Es un niño");
+	}
+  }
+
+     MEJOR OPCION//MAYOR EFICIENCIA
+
+    var edad;
+	edad=document.getElementById('edad').value;
+    edad=parseInt(edad);
+	if(edad>17)
+	{
+		alert("Usted es mayor de edad");
+
+	}
+    else
+    {
+    	if(edad<13)
+    	{
+    		alert("Es un niño");
+	
+    	}
+    	else
+    	{
+    		alert("Usted es un adolescente");
+	
+    	}
+	
+    }
+    */
+  
+
+
+
+
 //tomo la edad  
 
 
