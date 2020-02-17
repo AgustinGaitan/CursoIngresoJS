f9509583bb9c059a3accfa01ea2c0902f40fc412
jorge <asd@hotmail.com>
2020-02-16 23:24:57 -0300

    Ferrete Iluminación sin parseos demás

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -7,8 +7,89 @@ D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es de
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
- */
-function CalcularPrecio () 
+ 
+*/
+ function CalcularPrecio () 
 {
- 	
-}
+	var precioLampara;
+	var cantidadLampara;
+	var descuento;
+	var precioConDescuento;
+	var precioFinal;
+	var marca;
+	var precioLamparaSinDescuento;
+	var precioImpuesto;
+	var iibb; 
+
+	precioLampara=35;
+	descuento=0;
+	cantidadLampara=document.getElementById('Cantidad').value;
+	cantidadLampara=parseInt(cantidadLampara);
+	marca=document.getElementById('Marca').value;
+	precioLamparaSinDescuento=precioLampara*cantidadLampara;	
+//ITEM A//
+	if(cantidadLampara>5)
+	{
+		descuento=50;
+	}else
+	{
+//ITEM B//
+	
+		if(cantidadLampara==5)
+		{
+			if(marca=="ArgentinaLuz")
+			{
+				descuento=40;
+			} else
+			{
+				descuento=30;
+			}
+		}//ITEM C//
+		else
+		{
+			if(cantidadLampara==4)
+			{
+				if(marca=="ArgentinaLuz"||marca=="FelipeLamparas")
+				{
+					descuento=25;
+				}else
+				{
+					descuento=20;
+				}//ITEM D//
+			}else
+			{
+				if(cantidadLampara==3)
+				{
+					if(marca=="ArgentinaLuz")
+					{
+						descuento=15;
+					}else
+					{
+						if(marca=="FelipeLamparas")
+						{
+							descuento=10;
+						}else
+						{
+							descuento=5;
+						}
+						//ITEM E//
+					}
+						
+				}
+
+			}
+			
+		}	
+	}	
+	precioConDescuento=precioLamparaSinDescuento*descuento/100;
+	precioFinal=(cantidadLampara*precioLampara)-precioConDescuento;
+	iibb=precioFinal*10/100;
+	if(precioFinal>120)
+	{
+		precioImpuesto=precioFinal+iibb;
+		precioFinal=precioImpuesto
+		alert("Usted pagó $" +iibb+ " de impuestos");
+     }
+	document.getElementById('precioDescuento').value=precioFinal;
+}	
+ 
