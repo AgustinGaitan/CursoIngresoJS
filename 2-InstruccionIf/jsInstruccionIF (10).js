baf3e8ea987442eee85e1d1f2d7748a6c04f077a
jorge <asd@hotmail.com>
2020-02-17 18:40:15 -0300

    Todo If terminado 100% funcional

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,24 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numeroRandom;
+	numeroRandom=Math.floor(Math.random() * 10) + 1;  
+	if(numeroRandom>8)
+	{
+		alert("Excelente");
+	}
+	else
+		{
+			if(numeroRandom>3)
+			{
+				alert("Aprobó");
+			}
+			else
+			{
+				alert("Vamos, la próxima se puede");
+			}
+		}
+		console.log(numeroRandom);
 	
 
 }//FIN DE LA FUNCIÓN
