1ae9e0b1d1769ecc8cefa0dbe57385e5ef80c93f
alumno <alumno>
2020-02-18 10:37:42 -0300

    Ferrete iluminacion forma optima y switch + ejercicios de switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,9 +1,27 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
+	switch(mesDelAño)
+	{
+		case "Febrero":
+		alert("Este mes tiene 29 dias");
+		break;
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre":
+		case "Diciembre":
+		alert("Este mes tiene 31 dias");
+		break;
+		default:
+		alert("Este mes tiene 30 dias");
+		break;
+	}
 	
 	
 
--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*4.	Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
@@ -12,85 +11,137 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
 */
  function CalcularPrecio () 
 {
-	var precioLampara;
-	var cantidadLampara;
+	var cantidadDeLamparas;
+	var marca;
+	var precioBruto;
 	var descuento;
 	var precioConDescuento;
-	var precioFinal;
-	var marca;
-	var precioLamparaSinDescuento;
-	var precioImpuesto;
-	var iibb; 
 
-	precioLampara=35;
-	descuento=0;
-	cantidadLampara=document.getElementById('Cantidad').value;
-	cantidadLampara=parseInt(cantidadLampara);
+	cantidadDeLamparas=document.getElementById('Cantidad').value;
+	cantidadDeLamparas=parseInt(cantidadDeLamparas);
 	marca=document.getElementById('Marca').value;
-	precioLamparaSinDescuento=precioLampara*cantidadLampara;	
-//ITEM A//
-	if(cantidadLampara>5)
+	precioBruto=cantidadDeLamparas*35;
+	console.info("Precio bruto",precioBruto);
+
+	switch(cantidadDeLamparas)
+	{
+		case 5:
+
+			switch(marca)
+			{
+				case "ArgentinaLuz":
+					descuento=40;
+				break;
+				default:
+					descuento=30
+			}
+
+			break;
+		case 4:	
+			switch(marca)
+			{
+				case:"ArgentinaLuz"
+				case:"FelipeLamparas"
+					descuento=25
+				break;
+				default:20
+			}
+			break;
+		case 3:
+			switch(marca)
+			{
+				case"ArgentinaLuz"
+				descuento=15
+				break;
+				case"FelipeLamparas"
+				descuento=10
+				break;
+				default:
+				descuento=5
+			}
+			break;
+		case 1:   					//hasta que no se pone break; lo toma como un solo case//
+		case 2:
+			descuento=0;
+			break;
+		default:
+			descuento=50;
+			break;			
+					
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+	descuento=0;
+
+	if(cantidadDeLamparas>5)
 	{
 		descuento=50;
-	}else
+	}
+	else
 	{
-//ITEM B//
-	
-		if(cantidadLampara==5)
+		if(cantidadDeLamparas==5)
 		{
 			if(marca=="ArgentinaLuz")
 			{
 				descuento=40;
-			} else
+			}
+			else
 			{
 				descuento=30;
 			}
-		}//ITEM C//
+		}
 		else
 		{
-			if(cantidadLampara==4)
+			if(cantidadDeLamparas==4)
 			{
 				if(marca=="ArgentinaLuz"||marca=="FelipeLamparas")
 				{
 					descuento=25;
-				}else
+				}
+				else
 				{
 					descuento=20;
-				}//ITEM D//
-			}else
+				}
+			}
+			else
 			{
-				if(cantidadLampara==3)
+				if(cantidadDeLamparas==3)
 				{
 					if(marca=="ArgentinaLuz")
 					{
 						descuento=15;
-					}else
+					}
+					else
 					{
 						if(marca=="FelipeLamparas")
 						{
 							descuento=10;
-						}else
+						}
+						else
 						{
 							descuento=5;
 						}
-						//ITEM E//
 					}
-						
-				}
+				}//if(cantidadDeLamparas==3)
+			}//if(cantidadDeLamparas==4)
+		}//if(cantidadDeLamparas==5)
+	}//if(cantidadDeLamparas>5)
 
-			}
-			
-		}	
-	}	
-	precioConDescuento=precioLamparaSinDescuento*descuento/100;
-	precioFinal=(cantidadLampara*precioLampara)-precioConDescuento;
-	iibb=precioFinal*10/100;
-	if(precioFinal>120)
-	{
-		precioImpuesto=precioFinal+iibb;
-		precioFinal=precioImpuesto
-		alert("Usted pagó $" +iibb+ " de impuestos");
-     }
-	document.getElementById('precioDescuento').value=precioFinal;
+
+	precioConDescuento=precioBruto-precioBruto*descuento/100;
+	console.info("Precio con descuento",precioConDescuento);
+	document.getElementById('precioDescuento').value=precioConDescuento;
+
+	
 }	
  
