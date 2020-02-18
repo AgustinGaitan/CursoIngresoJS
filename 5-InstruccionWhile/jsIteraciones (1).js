1145e7933b6b87fe74be4e73f55fd7e858605458
alumno <alumno>
2020-02-18 12:02:42 -0300

    Primer y segundo ejercicio de while

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,15 @@
 function mostrar()
 {
-	alert('iteración while');
+	//alert('iteración while');
+	var numero;
+	numero=0;
+
+	while(numero<11)
+	{
+		console.log("Numero " +numero);
+		numero++;
+	}
+
 
 
 }//FIN DE LA FUNCIÓN
