6d0240f5ee57a82ea1be1f80b44cd832651a2e33
jorge <asd@hotmail.com>
2020-02-20 18:31:59 -0300

    Tp rising BTL sin completar con errores

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,9 +4,32 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	var numeroIngresado;
-	
-	var respuesta='si';
+	var respuesta=0;
+
+	while(respuesta!="no")
+	{
+		respuesta=prompt("Ingrese un numero, o diga que no para terminar");
+		if(!isNaN(respuesta))
+		{
+			respuesta=parseInt(respuesta);
+			
+			if(respuesta>0)
+			{
+			
+			    positivo=respuesta+positivo;
+			}
+			else
+			{	
+				if(respuesta<0)
+					{
+						negativo=negativo*respuesta;
+					}
+
+			}
+
+
+		}
+	}
 
 
 document.getElementById('suma').value=positivo;
--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,75 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
+	var edad;
+	var sexo;
+	var estadoCivil;
+	var sueldoBruto;
+	var legajo;
+	var nacionalidad;
+
+		estadoCivil=parseInt(estadoCivil);
+		sueldoBruto=parseInt(sueldoBruto);
+		legajo=parseInt(legajo);
+        edad=prompt("Ingrese su edad", "De 18 a 90");
+
+
+		if(edad>17&&edad<91)
+		{
+			document.getElementById('Edad').value=edad;
+			sexo=prompt("Ingrese su sexo","'m' o 'f'");
+
+			switch(sexo)
+			{
+				case "m":
+				case "f":
+				document.getElementById('Sexo').value=sexo;
+				estadoCivil=prompt("Ingrese su estado civil: 1 para Soltero, 2 para casados, 3 para divorciados y 4 para viudos","1,2,3,4");
+				break;
+				
+				switch(estadoCivil)
+				{
+					case 1:
+					estadoCivil="Soltero/a";
+					case 2:
+					estadoCivil="Casado/a";
+					case 3:
+					estadoCivil="Divorciado/a";
+					case 4:
+					estadoCivil="Viudo/a";
+					document.getElementById('EstadoCivil').value=estadoCivil;
+					sueldoBruto=prompt("Ingrese su sueldo(no menor a $8000)");
+					break;
+					
+					while(sueldoBruto<8000)
+					{
+						sueldoBruto=prompt("Ingrese su sueldo(no menor a $8000)");
+						document.getElementById('Sueldo').value=sueldoBruto;
+						legajo=prompt("Ingrese su legajo, de 4 cifras, sin ceros a la izquierda")
+
+					 }
+						if(!isNaN(legajo));
+						{
+							document.getElementById('Legajo').value=legajo;
+							nacionalidad=prompt("Ingrese su nacionalidad, 'A' para argentinos, 'E' para extranjeros, 'N' para nacionalizados")
+
+							switch(nacionalidad)
+							{
+								case "a":
+								nacionalidad=="Argentino/a";
+								break;
+								case "e":
+								nacionalidad=="Extranjero/a";
+								break;
+								case "n":
+								nacionalidad=="Nacionalizado/a";
+								break;
+								document.getElementById('Nacionalidad').value=nacionalidad;
+							}
+						}
+					
+				}
+			}
+		}
  
 }
