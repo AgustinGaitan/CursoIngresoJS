277ffd4008245930b63eb2ea7aad283cd32a6270
jorge <asd@hotmail.com>
2020-02-26 20:13:54 -0300

    Ejercicio 5 For terminado

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,6 +1,15 @@
 function mostrar()
 {
+	var numeroIngresado;
+	var i;
 
+	numeroIngresado=prompt("Ingrese un número");
+	numeroIngresado=parseInt(numeroIngresado);
+
+	for(i=0;numeroIngresado!=9;i++)
+	{
+		numeroIngresado=prompt("Ingrese un número");
+	}
 
 
 
