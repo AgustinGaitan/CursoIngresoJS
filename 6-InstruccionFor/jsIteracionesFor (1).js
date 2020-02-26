d752a0cfd013f992590452348721c82b33501a98
alumno <alumno@utnfra.com.ar>
2020-02-26 10:27:19 -0300

    iteraciones for 1

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,27 @@
 function mostrar()
 {
+	var i;
+
+	/*contador=0;
+	do    //la diferencia entre el do while y el while es que el do se ejecuta al menos una vez
+	{
+		console.log(contador); //del 0 al 9
+		contador ++; // contador=contador+1;
+		console.log(contador); // del 1 al 10
+	}while(contador>10);
+
+	while(contador>10);
+	{
+		console.log(contador); //del 0 al 9
+		contador ++; // contador=contador+1;
+		//console.log(contador); // del 1 al 10
+	} */
+
+	 //al contador se lo puede encontrar como "i"
+
+	for(i=0;i<10;i++) // los 3 pasos del while estan dentro 
+	{
+		console.log("for: " +i);
+	}
 
 }
