723a1d85e179530661c96f5e342bd98badd14880
alumno <alumno@utnfra.com.ar>
2020-03-03 12:05:16 -0300

    Faltan pocas cosas

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,33 @@
 function mostrar()
-{
+{	
+	var marca;
+	var peso;
+	var temperaturaDeAlmacenamiento;
+	var contadorPeso;
+	var respuesta;
+
+	
+	do
+	{
+		do
+		{
+		marca=prompt("Ingrese la marca del producto");
+		}
+	do
+	{
+		prompt=peso("Ingrese el peso (debe ser entre 1 y 100)");
+		peso=parseInt(peso);
+	}while(isNaN(peso) || (!(peso<101 && peso>0)));
+
+	do
+	{	
+		temperaturaDeAlmacenamiento=prompt("Ingrese la temperatura del producto");
+		temperaturaDeAlmacenamiento=parseInt(temperaturaDeAlmacenamiento);
+
+	}while(isNaN(temperaturaDeAlmacenamiento) || temperaturaDeAlmacenamiento>30 && temperaturaDeAlmacenamiento<-30);
+	
+
+	respuesta=prompt("Desea continuar? escriba 'si' si asi lo desea");
+	}while(respuesta=="si");
 
 }
--- 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,105 @@
 function mostrar()
-{
+{	
+	var nombreDelEstudiante;
+	var edadDelEstudiante;
+	var sexoDelEstudiante;
+	var notaFinal;
+	var respuesta;
+	var contadorHombres;
+	var contadorHombresAprobados;
+	var promedioDeNotasDeMenoresDeEdad;
+	var cantidadDeMenoresDeEdad;
+	var promedioDeNotasDeAdolescentes;
+	var cantidadAdolescentes;
+	var cantidadDeMayores;
+	var promedioDeNotasDeMayores;
+	var promedioDeNotasDeHombres;
+	var promedioDeNotasDeMujeres;
+	var contadorDeMujeres;
+	var contadorNotaFinalMujeres;
+	var contadorNotaFinalHombres;
+	var sumaNotaFinalHombres;
+	var sumaNotaFinalMujeres;
+
+	contadorHombres=0;
+	contadorHombresAprobados=0;
+	cantidadDeMenoresDeEdad=0;
+	cantidadAdolescentes=0;
+	cantidadDeMayores=0;
+	contadorDeMujeres=0;
+	promedioDeNotasDeMujeres=0;
+	promedioDeNotasDeMayores=0;
+	promedioDeNotasDeHombres=0;
+	promedioDeNotasDeAdolescentes=0;
+	promedioDeNotasDeMenoresDeEdad=0;
+
+	do
+	{
+		nombreDelEstudiante=prompt("Ingrese el nombre");
+		do
+		{
+			edadDelEstudiante=prompt("Ingrese la edad");
+			edadDelEstudiante=parseInt(edadDelEstudiante);
+		}while(isNaN(edadDelEstudiante));
+
+		do
+		{
+			sexoDelEstudiante=prompt("Ingrese el sexo, 'f' para femenino, 'm' para masculino");
+		}while(!isNaN(sexoDelEstudiante) || sexoDelEstudiante!="f" && sexoDelEstudiante != "m");
+
+		switch(sexoDelEstudiante)
+		{
+			case "m":
+				contadorHombres++;
+				
+				break;
+			default:
+				contadorDeMujeres++;
+				break;
+		}
+		do
+		{
+			notaFinal=prompt("Ingrese la nota final");
+			notafinal=parseInt(notaFinal);
+		}while(isNaN(notaFinal));
+
+		if(notaFinal>4 && sexoDelEstudiante=="m")
+		{
+			contadorHombresAprobados++;
+		}
+
+		if(edadDelEstudiante<18)
+		{
+			cantidadDeMenoresDeEdad++;
+			
+		}else 
+		{
+			cantidadDeMayores++;
+		}
+		
+		if(edadDelEstudiante>12 && edadDelEstudiante<18)
+		{
+			cantidadAdolescentes++;
+		}
+		
+		console.log("El nombre es " +nombreDelEstudiante);
+		console.log("La edad es " +edadDelEstudiante);
+		console.log("El sexo es " +sexoDelEstudiante);
+		console.log("La nota final es " +notaFinal);
+		
+
+		respuesta=prompt("Quiere continuar? ingrese 'si'. En caso contrario, escriba otra cosa.");
+	}while(respuesta=="si");
+	
+
+	promedioDeNotasDeMenoresDeEdad=notaFinal/cantidadDeMenoresDeEdad;
+	promedioDeNotasDeMayores=notaFinal/cantidadDeMayores;
+	promedioDeNotasDeAdolescentes=notaFinal/cantidadAdolescentes;
+	promedioDeNotasDeHombres=notaFinal/contadorHombres;
+	promedioDeNotasDeMujeres=notaFinal/contadorDeMujeres;
+	document.write("<br> La cantidad de varones aprobados es" +contadorHombresAprobados);
+	document.write("<br> El promedio de notas de los menores de edad es " +promedioDeNotasDeMenoresDeEdad);
+	document.write("<br> El promedio de notas de los adolescentes es " +promedioDeNotasDeAdolescentes);
+	document.write("<br> El promedio de notas de los mayores de edad es " +promedioDeNotasDeMayores);
 
 }
